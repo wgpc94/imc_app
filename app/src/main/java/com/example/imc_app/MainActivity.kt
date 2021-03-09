dfcb0fd522bd73d0bec29cb2e501d30a6b53048a
Welington Gomes <Welingtonsc94@gmail.com>
2021-03-09 19:06:36 -0300

    implementado MainActivity

--- app/src/main/java/com/example/imc_app/MainActivity.kt
+++ app/src/main/java/com/example/imc_app/MainActivity.kt
@@ -10,7 +10,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setlisteners()
     }
-  
+    private fun setlisteners(){
+        calcularBTN.setOnClickListener(){
+            calcularImc(pesoEDT.text.toString(),alturaEDT.text.toString())
+        }
+    }
+    private fun calcularImc(peso: String, altura: String){
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if (peso != null && altura != null){
+            val imc = peso /(altura*altura)
+            tituloTXT.text = "Seu IMC:\n%.2f".format(imc)
+        }
+    }
 }
